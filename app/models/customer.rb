f2aa4cf4c7d0f34431bd8be8d6a7d97135909151
toroduque <toro.duque@gmail.com>
2014-04-24 13:59:01 -0500

    Legal link added

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,4 +2,9 @@ class Customer < ActiveRecord::Base
   belongs_to :code
 
   validates_presence_of :name, :last_name, :cc, :phone, :email, :code_id
+  validates :phone, length: { maximum: 12 }
+  validates :code_id, length: { maximum: 7 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+
 end
