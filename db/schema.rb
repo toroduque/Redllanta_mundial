e13a857b161ef462657bf9b1892a725307d5a1f4
toroduque <toro.duque@gmail.com>
2015-05-15 18:06:16 -0500

    remove column prize from code

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150514213628) do
+ActiveRecord::Schema.define(version: 20150515230523) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 20150514213628) do
     t.string   "code"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "prize"
   end
 
   create_table "customers", force: true do |t|
