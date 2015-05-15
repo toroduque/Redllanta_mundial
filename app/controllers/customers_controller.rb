f299ab096dac72eb346d543b5a2b1c58e0e087a8
toroduque <toro.duque@gmail.com>
2015-05-15 18:00:48 -0500

    db seed

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -52,7 +52,7 @@ class CustomersController < ApplicationController
   private
   # Use callbacks to share common setup or constraints between actions.
   def set_customer
-    @customer = Customer.find(params[:id])
+    @customer = Customer.find(params[":id"])
   end
 
   # Only allow a trusted parameter "white list" through.
